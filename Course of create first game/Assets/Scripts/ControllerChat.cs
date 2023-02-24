975fc5bd6b418f45764ad54197773652c71143dd
YanSasiskas <mokkimlg@gmail.com>
2023-02-24 21:44:28 +0100

    Init

---
+++ Course of create first game/Assets/Scripts/ControllerChat.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class ControllerChat : MonoBehaviour
+{
+    // Скорость перемещения персонажа
+    public float moveSpeed = 5f;
+    // Сила прыжка персонажа
+    public float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Управление влево и вправо
+        float moveDirection = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+
+        // Прыжок
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Проверяем, коснулся ли персонаж земли
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
---
+++ Course of create first game/Assets/Scripts/CourseScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CourseScript : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    [SerializeField] private float _speed;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private float _jumpPower;
+    [SerializeField] private Transform _groundChecker;
+    [SerializeField] private float _groundCheckerRadius;
+    [SerializeField] private LayerMask _whatIsGround;
+
+    private float _direction;
+    private bool _jump;
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _direction = Input.GetAxisRaw("Horizontal");
+        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _jump = true;
+            Debug.Log("Jump");
+        }
+        if (_direction > 0 && _spriteRenderer.flipX)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if (_direction < 0 && !_spriteRenderer.flipX)
+        {
+            _spriteRenderer.flipX = true;
+        }            
+    }
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = new Vector2(_direction * _speed, _rigidbody.velocity.y);
+        
+        bool canJump = Physics2D.OverlapCircle(_groundChecker.position, _groundCheckerRadius, _whatIsGround);
+        
+        if (_jump && canJump)
+        {
+            _rigidbody.AddForce(Vector2.up * _jumpPower);
+            _jump = false;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(_groundChecker.position, _groundCheckerRadius);
+    }
+}
